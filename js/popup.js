735bc48165913571b0d9c7100bf80aa7fa52de5c
Alexander Pankov <alexander.pankov88@mail.ru>
2020-06-25 20:29:07 +0700

    Задание 9

---
+++ js/popup.js
@@ -0,0 +1,56 @@
+var loginLink = document.querySelector(".main-contacts-link");
+var loginPopup = document.querySelector(".writeus");
+var loginClose = loginPopup.querySelector(".writeus-close");
+var loginForm = loginPopup.querySelector(".writeus-form");
+var loginLogin = loginPopup.querySelector(".writeus-input-name");
+var loginEmail = loginPopup.querySelector(".writeus-input-email");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+
+  if (storage) {
+    loginLogin.value = storage;
+    loginEmail.focus();
+  } else {
+    loginLogin.focus();
+  }
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginEmail.value) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-error");
+    loginPopup.offsetWidth = loginPopup.offsetWidth;
+    loginPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+      loginPopup.classList.remove("modal-error");
+    }
+  }
+});
